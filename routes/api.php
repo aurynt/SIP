efa2da8268abd9707b2258c6e34a3b2710662f7f
DoniApriano <dapriano795@gmail.com>
2024-02-15 09:37:39 +0700

    feat: signup

--- routes/api.php
+++ routes/api.php
@@ -24,6 +24,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::post('/auth/login', [AuthController::class, 'login']);
+Route::post('/auth/register', [AuthController::class, 'register']);
 
 Route::middleware(['auth:sanctum'])->group(function () {
 
