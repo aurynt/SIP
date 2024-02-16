d3c1fe40fd301bb954e476654ac97ee0c40f8cd0
ZEPEYUS <anzocraft@gmail.com>
2024-02-16 14:04:18 +0700

    fixing-map

--- resources/views/admin/layouts/navbar.blade.php
+++ resources/views/admin/layouts/navbar.blade.php
@@ -19,7 +19,11 @@
                 <li class="nav-item d-flex align-items-center">
                     <a href="javascript:;" class="nav-link text-body font-weight-bold px-0">
                         <i class="fa fa-user me-sm-1"></i>
-                        <span class="d-sm-inline d-none">Sign In</span>
+                        @auth
+                            <span class="d-sm-inline d-none">Sign In</span>
+                        @else
+                            <span class="d-sm-inline d-none">Sign In</span>
+                        @endauth
                     </a>
                 </li>
                 <li class="nav-item d-xl-none ps-3 d-flex align-items-center">
